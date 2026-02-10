376016c2ef48cc77464f77ed4c9f9c72d6bc8ded
Reivaxmar <matemusicpro@outlook.com>
2026-02-10 21:21:12 +0100

    Added bf compiler

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Project title
-This is just a template for making projects. Feel free to use.
+# Barinfrick compiler
+Brainfuck compiler, but made really fast and with spelling mistakes
 
 ## Usage
 ### Prerequisites
@@ -10,30 +10,18 @@ Just some basic stuff:
 ### Building the project
 You can download a zip file or use git:
 ```bash
-> mkdir ./project
-> cd ./project
 > git clone \
-    ADD_GIT_LINK \
-    project_name
-> cd project_name
+    https://github.com/Reivaxmar/Barinfrick-compiler \
+    Barinfrick-compiler
+> cd Barinfrick-compiler
 ```
 
 Great! Now you have the project, now you just need to build it:
 ```bash
 > bash ./build.sh
-> make
-> ./proj
+> cmake --build .
+> ./barinfrick
 Hello, world!
 ```
 
 Now your project should be up and running.
-
-
-## External libraries
-There is no need to have them installed, CMake will do it:
-- Dep1
-- Dep2
-
-## TODOs
-- [x] Implement hello world
-- [ ] Do everything else
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
-#include "header.h"
+#include <fstream>
+#include <string>
+#include <cstdint>
+using namespace std;
+
+#define MAXN 30000
 
 int main() {
-    std::cout << a_cool_string() << std::endl;
+    ifstream file("assets/file.bf");
+
+    string s;
+    string program;
+    char chars[] = {'<', '>', '+', '-', '.', ',', '[', ']'};
+    while(getline(file, s)) {
+        for(char c : s) {
+            for(auto x : chars) {
+                if(c == x) program += c;
+            }
+        }
+    }
+
+    uint8_t ar[MAXN] = {0};
+
+    auto ptr = ar;
+    int n = program.size();
+
+    for(int cur = 0; cur < n; cur++) {
+        char c = program[cur];
+        
+        if(c == '>') ptr++;
+        else if(c == '<') ptr--;
+        else if(c == '+') (*ptr)++;
+        else if(c == '-') (*ptr)--;
+        else if(c == '.') cout << *ptr;
+        else if(c == ',') cin >> *ptr;
+        else if(c == '[') {
+            if(ptr == 0) {
+                int loop = 1;
+                while(loop > 0) {
+                    cur++;
+                    if(cur >= n) break;
+                    if(program[cur] == '[') loop++;
+                    else if(program[cur] == ']') loop--;
+                }
+            }
+        }
+        else if(c == ']') {
+            if(*ptr != 0) {
+                int loop = 1;
+                while(loop > 0) {
+                    cur--;
+                    if(cur < 0) break;
+                    if(program[cur] == '[') loop--;
+                    else if(program[cur] == ']') loop++;
+                }
+            }
+        }
+    }
 }
